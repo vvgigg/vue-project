96f5d1f40f29d5e7d447799761e47c9f856b1fbb
Vipada Varasajja <vipada@MacBook-Air-khxngVipada.local>
2025-05-07 22:26:09 +0700

    First commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0) // สร้างตัวแปร count ด้วย ref และค่าเริ่มต้นเป็น 0
+</script>
+
+<template>
+  <div>
+    <p>จำนวน: {{ count }}</p>
+    <button @click="count++">add</button> <!-- กดปุ่มเพิ่มค่า -->
+  </div>
+</template>
+
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
